9b04003a0f56be1b30292bd32a335b9fdb455fb8
kent-3 <100624004+kent-3@users.noreply.github.com>
2022-06-06 21:09:53 -0400

    saving just in case

--- examples/schema.rs
+++ examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use secret_contract_example::msg::{HandleMsg, InitMsg, QueryMsg, HandleAnswer, QueryAnswer};
-use secret_contract_example::state::Config;
+use metadata_provider_contract::msg::{HandleMsg, InitMsg, QueryMsg, HandleAnswer, QueryAnswer};
+use metadata_provider_contract::state::Config;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
